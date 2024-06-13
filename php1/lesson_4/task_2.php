52032a3d11d36ad06a3ade9d4809618d60b06354
Wendia <wendia1234qwer@mail.ru>
2024-06-14 00:31:39 +0300

    complete 4 lesson in php_1

---
+++ php1/lesson_4/task_2.php
@@ -0,0 +1,37 @@
+<?php
+$images = scandir(__DIR__ . '/images');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title> Lesson 3 </title>
+</head>
+<body>
+<div class="container">
+    <h3 class="display-3 text-center mb-5"> КотоГалерея </h3>
+
+    <div class="row justify-content-center g-4 mb-4">
+        <?php
+        foreach ($images as $key => $image) {
+            if ($image != '.' && $image != '..') { ?>
+                <div class="col-auto">
+                    <a href="/image.php?id=<?php echo $key; ?>">
+                        <img width="200" height="200" class="object-fit-cover"
+                             src="/images/<?php echo $image; ?>" alt="cat">
+                    </a>
+                </div>
+            <?php }
+        } ?>
+    </div>
+
+    <h4 class="display-4 text-center mb-5"> Загрузи своего котика </h4>
+
+    <form action="/upload_img.php" method="post" enctype="multipart/form-data">
+        <input class="form-control mb-3" type="file" name="img" accept="image/png, image/jpeg"/>
+        <input class="btn btn-primary" type="submit">
+    </form>
+</div>
+</body>
+</html>
---
+++ php1/lesson_4/upload_img.php
@@ -0,0 +1,27 @@
+<?php
+
+$images = scandir(__DIR__ . '/images');
+
+if (isset($_FILES['img']) && empty($_FILES['img']['error'])) {
+    $imgPrefix = 1;
+    $path = __DIR__ . '/images';
+    $imageName = $_FILES['img']['name'];
+
+// Вариант 1
+ date_default_timezone_set('Europe/Moscow');
+ move_uploaded_file($_FILES['img']['tmp_name'], $path . '/' . time() . '_' . $_FILES['img']['name']);
+
+// Вариант 2
+//    while (in_array($imageName, $images)) {
+//        $imageName =
+//            pathinfo($_FILES['img']['name'])['filename'] .
+//            '_' . $imgPrefix . '.' .
+//            pathinfo($_FILES['img']['name'])['extension'];
+//        $imgPrefix++;
+//    }
+//    move_uploaded_file($_FILES['img']['tmp_name'], $path . '/' . $imageName);
+}
+
+header('Location: /task_2.php');
+exit;
+
